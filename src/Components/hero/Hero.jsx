6ae96362c3a7104d6c1fbcf8be7c9a3d4b365d3c
712Kunal <jadhavkun0@gmail.com>
2024-08-09 00:20:02 +0530

    Updated the Resume

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -49,7 +49,7 @@ const handleSubmit = () => {
 }
 
 const handleResume = () => {
-    window.open("", "_blank");
+    window.open("https://drive.google.com/file/d/1SaMvP0iuF7W8_Rak31GXx0hqN0xOtpYQ/view?usp=drive_link", "_blank");
 }
 
 const handlescroll = () => {
